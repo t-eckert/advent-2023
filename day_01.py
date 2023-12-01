f8818805e3c01cbdc5688d0435ba3443a0515a2d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-01 17:35:35 -0500

    Refactor the day 1 solution

--- day_01.py
+++ day_01.py
@@ -1,30 +1,60 @@
+""" Advent of Code 2023, Day 1
+
+"""
+
 from lib import read
 
 
+################################################################################
+# Part 1 #######################################################################
+################################################################################
 def part_1(lines: list[str]) -> int:
-    sum = 0
-    for line in lines:
-        first = None
-        last = None
-        for c in line:
-            if is_number(c):
-                first = c
-                break
-        for c in line[::-1]:
-            if is_number(c):
-                last = c
-                break
-        assert first
-        assert last
-        sum += int(first + last)
-    return sum
+    return sum([int(first_number(line) + first_number(line[::-1])) for line in lines])
 
 
+def is_number(c: str) -> bool:
+    return c in "0123456789"
+
+
+def first_number(line: str) -> str:
+    for c in line:
+        if is_number(c):
+            return c
+    return ""
+
+
+################################################################################
+# Part 2 #######################################################################
+################################################################################
 def part_2(lines: list[str]) -> int:
-    sum = 0
-    for line in lines:
-        sum += get_numbers_with_words(line)
-    return sum
+    return sum(
+        [int(first_number_or_word(line) + last_number_or_word(line)) for line in lines]
+    )
+
+
+def first_number_or_word(line: str) -> str:
+    for i, c in enumerate(line):
+        if is_number(c):
+            return c
+        if matches := number_words.get(c):
+            for number_word in matches:
+                word = line[i : i + len(number_word)]
+                if word == number_word:
+                    return number_words[c][number_word]
+    return ""
+
+
+def last_number_or_word(line: str) -> str:
+    for i, c in enumerate(line[::-1]):
+        if is_number(c):
+            return c
+        if matches := number_words.get(c):
+            for number_word in matches:
+                offset = len(line) - i - 1
+                word = line[offset : offset + len(number_word)]
+                if word == number_word:
+                    return number_words[c][number_word]
+    return ""
 
 
 number_words: dict[str, dict[str, str]] = {
@@ -45,56 +75,6 @@ number_words: dict[str, dict[str, str]] = {
     "e": {"eight": "8"},
     "n": {"nine": "9"},
 }
-
-
-def is_number(c: str) -> bool:
-    return c in "0123456789"
-
-
-def get_numbers_with_words(line: str) -> int:
-    print("=========")
-    print(line)
-    first = None
-    last = None
-
-    # Find the first number or number word moving in from the left.
-    for i, c in enumerate(line):
-        if is_number(c):
-            first = c
-            break
-        if matches := number_words.get(c):
-            for number_word in matches:
-                if line[i : len(number_word)] == number_word:
-                    first = number_words[c].get(number_word)
-                break
-            if first:
-                break
-
-    # Find the last number or number word moving in from the right.
-    for i, c in enumerate(line[::-1]):
-        print(i, c)
-        if is_number(c):
-            last = c
-            break
-        if matches := number_words.get(c):
-            print(matches)
-            for number_word in matches:
-                offset = len(line) - i - 1
-                word = line[offset : offset + len(number_word)]
-                print(word)
-                if word == number_word:
-                    print("matched!")
-                    last = number_words[c].get(number_word)
-                    print(last)
-                break
-            if last:
-                break
-
-    assert first
-    assert last
-    print(line, "|", first, last)
-
-    return int(first + last)
 
 
 if __name__ == "__main__":
