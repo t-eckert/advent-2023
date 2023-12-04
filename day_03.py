50e429fb1f133102d6b32b352f67321540fd5e61
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-03 22:42:30 -0500

    Removed some older parts of Day 3

--- day_03.py
+++ day_03.py
@@ -31,6 +31,7 @@ def part_1(engine_rows: list[str], symbols: set[str]) -> int:
 
     return total
 
+
 def part_2(engine_rows: list[str]) -> int:
     engine_dimensions = (len(engine_rows), len(engine_rows[0]))
 
@@ -48,13 +49,14 @@ def part_2(engine_rows: list[str]) -> int:
                 reading_number = True
             else:
                 if reading_number:
-                    numbers.append(PartNumber(int(number_buffer), subject_buffer.copy()))
+                    numbers.append(
+                        PartNumber(int(number_buffer), subject_buffer.copy())
+                    )
                     subject_buffer.clear()
                     number_buffer = ""
                 reading_number = False
 
     gears = {}
-
     for number in numbers:
         for neighbor in border(number.cells, engine_dimensions):
             if engine_rows[neighbor[0]][neighbor[1]] == "*":
@@ -72,69 +74,12 @@ def part_2(engine_rows: list[str]) -> int:
 
 
 class PartNumber:
-    def __init__(self, value: int, cells: set[tuple[int,int]]):
+    def __init__(self, value: int, cells: set[tuple[int, int]]):
         self.value = value
         self.cells = cells
 
     def __repr__(self) -> str:
         return f"{self.value}\t{self.cells=}"
-
-
-def get_ratio(engine_rows: list[str], rows: list[set[tuple[int, int]]]) -> int:
-    ratio = 1
-    for row in rows:
-        if are_contiguous(row):
-            ratio *= glob_number(engine_rows, row.pop())
-        else:
-            cells = [cell for cell in row]
-            a, b = cells[0], cells[1]
-            ratio *= glob_number(engine_rows, a)
-            ratio *= glob_number(engine_rows, b)
-
-    return ratio
-
-
-def glob_number(engine_rows: list[str], cell: tuple[int, int]) -> int:
-    row, col = cell[0], cell[1]
-    number = engine_rows[row][col]
-
-    # Back to start
-    for c in range(col - 1, 0, -1):
-        val = engine_rows[row][c]
-        if val not in digits:
-            break
-        number = val + number
-    # To the end
-    for c in range(col + 1, len(engine_rows[row])):
-        val = engine_rows[row][c]
-        if val not in digits:
-            break
-        number += val
-
-    return int(number)
-
-
-def group_rows(cells: set[tuple[int, int]]) -> list[set[tuple[int, int]]]:
-    row_groups = {}
-    for cell in cells:
-        if cell[0] not in row_groups.keys():
-            row_groups[cell[0]] = [cell]
-        else:
-            row_groups[cell[0]].append(cell)
-
-    return [group for group in row_groups.values()]
-
-
-def are_contiguous(cells: set[tuple[int, int]]) -> bool:
-    # Numbers can only be contiguous within rows.
-    cols = sorted([cell[1] for cell in cells])
-
-    prev = None
-    for col in cols:
-        if prev and prev != col - 1:
-            return False
-        prev = col
-    return True
 
 
 def has_symbol(
@@ -151,49 +96,30 @@ def border(
 ) -> set[tuple[int, int]]:
     height, width = engine_dimensions
 
-    all_borders = set()
+    borders = set()
 
-    cell_borders = set()
     for cell in subject:
-        cell_borders.clear()
-
         row, col = cell[0], cell[1]
 
         # Iterate clockwise around the location
         if row > 0 and col > 0:
-            cell_borders.add((row - 1, col - 1))  # above left
+            borders.add((row - 1, col - 1))  # above left
         if row > 0:
-            cell_borders.add((row - 1, col))  # above center
+            borders.add((row - 1, col))  # above center
         if row > 0 and col < width - 1:
-            cell_borders.add((row - 1, col + 1))  # above right
+            borders.add((row - 1, col + 1))  # above right
         if col < width - 1:
-            cell_borders.add((row, col + 1))  # center right
+            borders.add((row, col + 1))  # center right
         if row < height - 1 and col < width - 1:
-            cell_borders.add((row + 1, col + 1))  # below right
+            borders.add((row + 1, col + 1))  # below right
         if row < height - 1:
-            cell_borders.add((row + 1, col))  # below center
+            borders.add((row + 1, col))  # below center
         if row < height - 1 and col > 0:
-            cell_borders.add((row + 1, col - 1))  # below left
+            borders.add((row + 1, col - 1))  # below left
         if col > 0:
-            cell_borders.add((row, col - 1))  # center left
+            borders.add((row, col - 1))  # center left
 
-        all_borders.update(cell_borders)
-
-    return all_borders - subject
-
-
-def border_test():
-    tests = [
-        ({(0, 0)}, (4, 4), {(1, 0), (1, 1), (0, 1)}),
-        (
-            {(1, 1), (1, 2), (1, 3)},
-            (4, 4),
-            {(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (2, 0), (2, 1), (2, 2), (2, 3)},
-        ),
-    ]
-
-    for test in tests:
-        assert test[2] == border(test[0], test[1])
+    return borders - subject
 
 
 if __name__ == "__main__":
@@ -213,3 +139,16 @@ if __name__ == "__main__":
     print(part_2(engine_rows))
     print()
 
+
+def border_test():
+    tests = [
+        ({(0, 0)}, (4, 4), {(1, 0), (1, 1), (0, 1)}),
+        (
+            {(1, 1), (1, 2), (1, 3)},
+            (4, 4),
+            {(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (2, 0), (2, 1), (2, 2), (2, 3)},
+        ),
+    ]
+
+    for test in tests:
+        assert test[2] == border(test[0], test[1])
