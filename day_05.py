ebe19d91ad22d1557b11829d0ad3e517e53e953a
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-05 10:02:52 -0500

    Add base for day 5

---
+++ day_05.py
@@ -0,0 +1,18 @@
+from lib import read, tee
+
+def part_1(almanac: str) -> int:
+    return 0
+
+def part_2(almanac: str) -> int:
+    return 0
+
+if __name__ == "__main__":
+    almanac = read("day_05.txt").strip()
+
+    print("Part 1")
+    print(part_1(almanac))
+    print()
+
+    print("Part 2")
+    print(part_2(almanac))
+    print()
