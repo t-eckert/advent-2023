f17953bf2488a12432326996e00963cac027c72a
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-04 21:34:36 -0500

    Day 4 Part 2

--- day_04.py
+++ day_04.py
@@ -1,30 +1,34 @@
 from lib import read, tee
 from collections import namedtuple
 
-Card = namedtuple("Card", "mine winning")
+Card = namedtuple("Card", "id mine winning")
 
 def part_1(cards: list[str]) -> int:
     return sum(int(2 ** (count_winning(deserialize_card(card)) - 1)) for card in cards)
 
 
-def part_2(cards: list[str]) -> int:
-    """
-    Each card wins you more cards
+def part_2(raw_input: list[str]) -> int:
+    tally = []
 
-    """
+    cards = [deserialize_card(raw) for raw in raw_input[::-1]]
+    for card in cards:
+        wins = count_winning(card)
+        won_cards = 0
+        if 0 < wins:
+            won_cards = sum(tally[-wins:])
+        tally.append(1 + won_cards)
+
+    return sum(tally)
 
 
-
-    return 0
-
-
-def count_winning(number_sets: Card) -> int:
-    return len(number_sets.mine.intersection(number_sets.winning))
+def count_winning(card: Card) -> int:
+    return len(card.mine.intersection(card.winning))
 
 
-def deserialize_card(card: str) -> Card:
-    mine, winning = card.split(":")[1].strip().split("|")
-    return Card(to_set(mine), to_set(winning))
+def deserialize_card(raw: str) -> Card:
+    id = raw.split(":")[0]
+    mine, winning = raw.split(":")[1].strip().split("|")
+    return Card(id,to_set(mine), to_set(winning))
 
 
 def to_set(numbers: str) -> set[int]:
