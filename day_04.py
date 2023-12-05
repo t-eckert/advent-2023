fdfdf02ff69c60a3822191ad026c5d074463ce5a
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-04 20:52:43 -0500

    Add Card tuple

--- day_04.py
+++ day_04.py
@@ -1,21 +1,30 @@
 from lib import read, tee
+from collections import namedtuple
 
+Card = namedtuple("Card", "mine winning")
 
 def part_1(cards: list[str]) -> int:
     return sum(int(2 ** (count_winning(deserialize_card(card)) - 1)) for card in cards)
 
 
 def part_2(cards: list[str]) -> int:
+    """
+    Each card wins you more cards
+
+    """
+
+
+
     return 0
 
 
-def count_winning(number_sets: tuple[set[int], set[int]]) -> int:
-    return len(number_sets[0].intersection(number_sets[1]))
+def count_winning(number_sets: Card) -> int:
+    return len(number_sets.mine.intersection(number_sets.winning))
 
 
-def deserialize_card(card: str) -> tuple[set[int], set[int]]:
+def deserialize_card(card: str) -> Card:
     mine, winning = card.split(":")[1].strip().split("|")
-    return (to_set(mine), to_set(winning))
+    return Card(to_set(mine), to_set(winning))
 
 
 def to_set(numbers: str) -> set[int]:
