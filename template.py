dff5d04264ee734951c31aca1f65ee8586dc8462
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-07 20:54:05 -0500

    Add template

---
+++ template.py
@@ -0,0 +1,18 @@
+from lib import read, tee
+
+file = "day_00.txt"
+
+def part_1(puzzle: list[str]) -> int:
+    return 0
+
+
+def part_2(puzzle: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    puzzle = read(file).strip().splitlines()
+
+    print(f"Part 1: {part_1(puzzle)}")
+    print(f"Part 2: {part_2(puzzle)}")
+
